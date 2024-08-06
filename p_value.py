a6a345fa662e71242fe1d64433d54ea7715e8eb1
Lorenzo <lorenzo.allasia774@edu.unito.it>
2024-08-06 15:04:44 +0200

    Update p_value.py

--- p_value.py
+++ p_value.py
@@ -3,12 +3,20 @@ import ROOT
 fTot = ROOT.TFile("Tfile/tot_pdf.root")
 tot = fTot.Get("ws_totPDF")
 
+fData = ROOT.TFile("Tfile/data_gen.root")
+d = fData.Get("ws_data")
+
 tot.var("alpha_L").setConstant(1)
 tot.var("alpha_R").setConstant(1)
 tot.var("n_L").setConstant(1)
 tot.var("n_R").setConstant(1)
 tot.var("sigma_L").setConstant(1)
 tot.var("sigma_R").setConstant(1)
+
+scale_factor = d.var("scale_factor")
+
+tot.var("lumi").setVal(40*pow(10,15)*scale_factor.getVal())
+tot.var("Nbkg").setVal(586*scale_factor.getVal())
 
 #Set the RooModelConfig and let it know what the content of the workspace is about
 model = ROOT.RooStats.ModelConfig()
@@ -25,7 +33,7 @@ nullParams.setRealValue("BR_H",0.)
 #Build the profile likelihood calculator
 plc = ROOT.RooStats.ProfileLikelihoodCalculator()
 
-plc.SetData(tot.data("data"))
+plc.SetData(d.data("data"))
 plc.SetModel(model)
 plc.SetParameters(poi)
 plc.SetNullParameters(nullParams)
@@ -37,5 +45,3 @@ print "-------------------------------------------------"
 print "The p-value for the null is ", htr.NullPValue()
 print "Corresponding to a signifcance of ", htr.Significance()
 print "-------------------------------------------------"
-
-

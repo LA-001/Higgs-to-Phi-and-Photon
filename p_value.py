d9cf8e9a9739927d08af0becbdf0237d61e0189b
Lorenzo <lorenzo.allasia774@edu.unito.it>
2024-09-06 10:06:12 +0200

    Update p_value.py

--- p_value.py
+++ p_value.py
@@ -1,31 +1,31 @@
 import ROOT
 
 fTot = ROOT.TFile("Tfile/tot_pdf.root")
-tot = fTot.Get("ws_totPDF")
+f = fTot.Get("ws_totPDF")
 
 fData = ROOT.TFile("Tfile/data_gen.root")
 d = fData.Get("ws_data")
 
-tot.var("alpha_L").setConstant(1)
-tot.var("alpha_R").setConstant(1)
-tot.var("n_L").setConstant(1)
-tot.var("n_R").setConstant(1)
-tot.var("sigma_L").setConstant(1)
-tot.var("sigma_R").setConstant(1)
+f.var("alpha_L").setConstant(1)
+f.var("alpha_R").setConstant(1)
+f.var("n_L").setConstant(1)
+f.var("n_R").setConstant(1)
+f.var("sigma_L").setConstant(1)
+f.var("sigma_R").setConstant(1)
 
 scale_factor = d.var("scale_factor")
 
-tot.var("lumi").setVal(40*pow(10,15)*scale_factor.getVal())
-tot.var("Nbkg").setVal(586*scale_factor.getVal())
+f.var("lumi").setVal(40*pow(10,15)*scale_factor.getVal())
+f.var("Nbkg").setVal(586*scale_factor.getVal())
 
 #Set the RooModelConfig and let it know what the content of the workspace is about
 model = ROOT.RooStats.ModelConfig()
-model.SetWorkspace(tot)
+model.SetWorkspace(f)
 model.SetPdf("totPDF")
 
 #Here we explicitly set the value of the parameters for the null hypothesis
 #We want no signal contribution, so BR_H = 0
-BR_H = tot.var("BR_H")
+BR_H = f.var("BR_H")
 poi = ROOT.RooArgSet(BR_H)
 nullParams = poi.snapshot()
 nullParams.setRealValue("BR_H",0.)
